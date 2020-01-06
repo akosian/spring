74ee7cb4f492bcb1f5eab35daa05139a162da195
a.kosyan <a.kosyan@a1qa.com>
2020-01-06 21:38:50 +0000

    Implement Security && mysql connection

--- src/main/java/sweater/GreetingController.java
+++ src/main/java/sweater/GreetingController.java
@@ -1,16 +1,50 @@
 package sweater;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sweater.domain.Message;
+import sweater.repos.MessageRepository;
+
+import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String addMessage(Map<String, Object> model, @RequestParam String text, @RequestParam String tag) {
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filterByTag(@RequestParam String tag, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (tag != null || !tag.isEmpty()) {
+            messages = messageRepository.findByTag(tag);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.put("messages", messages);
+        return "main";
     }
 }
---
+++ src/main/java/sweater/domain/Message.java
@@ -0,0 +1,29 @@
+package sweater.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+}
